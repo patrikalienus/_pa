fc7dbf687a198091fdbf65da956966f359be5871
PA <patrik.alienus@sparkhouse.se>
2019-12-24 19:31:10 +0100

    misc

--- wp-content/themes/_pa/functions.php
+++ wp-content/themes/_pa/functions.php
@@ -7,19 +7,20 @@
  * @package _pa
  */
 
-define('BUILD', get_stylesheet_directory_uri() . '/_build' );
-define('JS', 	BUILD . '/js' );
-define('CSS', 	BUILD . '/css' );
-define('IMG', 	BUILD . '/images' );
-define('FONTS', BUILD . '/webfonts' );
+define('BUILD', 	get_stylesheet_directory_uri() . '/_build' );
+define('JS', 		BUILD . '/js' );
+define('CSS', 		BUILD . '/css' );
+define('IMG', 		BUILD . '/images' );
+define('FONTS', 	BUILD . '/webfonts' );
 
-require_once( get_template_directory() . '/_functions/global.php');
-require_once( get_template_directory() . '/_functions/widgets.php');
-require_once( get_template_directory() . '/_functions/scripts_styles.php');
-require_once( get_template_directory() . '/_functions/class-tgm-plugin-activation.php');
-require_once( get_template_directory() . '/_functions/tgmpa.php' );
-require_once( get_template_directory() . '/_functions/custom-header.php');
-require_once( get_template_directory() . '/_functions/template-tags.php');
-require_once( get_template_directory() . '/_functions/template-functions.php');
-require_once( get_template_directory() . '/_functions/customizer.php');
-require_once( get_template_directory() . '/_functions/jetpack.php');
+define('FUNCTIONS', get_template_directory() . '/_functions');
+require_once( FUNCTIONS . '/global.php');
+require_once( FUNCTIONS . '/widgets.php');
+require_once( FUNCTIONS . '/scripts_styles.php');
+require_once( FUNCTIONS . '/class-tgm-plugin-activation.php');
+require_once( FUNCTIONS . '/tgmpa.php' );
+require_once( FUNCTIONS . '/custom-header.php');
+require_once( FUNCTIONS . '/template-tags.php');
+require_once( FUNCTIONS . '/template-functions.php');
+require_once( FUNCTIONS . '/customizer.php');
+require_once( FUNCTIONS . '/jetpack.php');
